5ebd624eb1749270d4df087ec197443a2dae5a10
progammerBoy <sy8692935@gmail.com>
2026-06-25 12:09:15 +0530

    add origins

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ const bookingRoutes = require('./routes/bookings');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Routes
